e1abe980a9e4b63ae57d59f75ced218a00fca373
yexiaochuan <tap91624@gmail.com>
2024-01-24 23:17:05 +0800

    Annotate cmake

---
+++ tao_pointpillars/tensorrt_sample/test/build/CMakeFiles/pointpillars.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/code/2/tao_toolkit_recipes/tao_pointpillars/tensorrt_sample/src/pointpillar.cpp" "CMakeFiles/pointpillars.dir/code/2/tao_toolkit_recipes/tao_pointpillars/tensorrt_sample/src/pointpillar.cpp.o" "gcc" "CMakeFiles/pointpillars.dir/code/2/tao_toolkit_recipes/tao_pointpillars/tensorrt_sample/src/pointpillar.cpp.o.d"
+  "/code/2/tao_toolkit_recipes/tao_pointpillars/tensorrt_sample/src/postprocess.cpp" "CMakeFiles/pointpillars.dir/code/2/tao_toolkit_recipes/tao_pointpillars/tensorrt_sample/src/postprocess.cpp.o" "gcc" "CMakeFiles/pointpillars.dir/code/2/tao_toolkit_recipes/tao_pointpillars/tensorrt_sample/src/postprocess.cpp.o.d"
+  "/code/2/tao_toolkit_recipes/tao_pointpillars/tensorrt_sample/test/main.cpp" "CMakeFiles/pointpillars.dir/main.cpp.o" "gcc" "CMakeFiles/pointpillars.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ tao_pointpillars/tensorrt_sample/test/build/CMakeFiles/pointpillars.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pointpillars.dir/code/2/tao_toolkit_recipes/tao_pointpillars/tensorrt_sample/src/pointpillar.cpp.o"
+  "CMakeFiles/pointpillars.dir/code/2/tao_toolkit_recipes/tao_pointpillars/tensorrt_sample/src/pointpillar.cpp.o.d"
+  "CMakeFiles/pointpillars.dir/code/2/tao_toolkit_recipes/tao_pointpillars/tensorrt_sample/src/postprocess.cpp.o"
+  "CMakeFiles/pointpillars.dir/code/2/tao_toolkit_recipes/tao_pointpillars/tensorrt_sample/src/postprocess.cpp.o.d"
+  "CMakeFiles/pointpillars.dir/main.cpp.o"
+  "CMakeFiles/pointpillars.dir/main.cpp.o.d"
+  "pointpillars"
+  "pointpillars.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pointpillars.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
